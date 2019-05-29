2405e64f7f4131019800e5f14b56e7288ea4815f
Richard Hpa <richard.hpa@hotmail.com>
2019-05-30 09:20:40 +1200

    added all 4 click events

--- js/script.js
+++ js/script.js
@@ -21,19 +21,56 @@ $(document).ready(function(){
     //     console.log('timeout has been cleared');
     // }, 10000)
 
+    var timer;
+    var seconds = 0;
+    var minutes = 0;
 
     $('#start').click(function(){
-        var seconds = 0;
-        setInterval(function(){
-            // console.log('this is happening every second');
-            seconds++;
-            console.log(seconds);
-            $('#seconds').text(seconds);
-        }, 1000);
+        $(this).hide();
+        $('#pause').removeClass('d-none').show();
+        setTimerInterval();
     });
 
+    $('#pause').click(function(){
+        $(this).hide();
+        $('#continue').removeClass('d-none').show();
+        $('#reset').removeClass('d-none').show();
+        clearInterval(timer);
+    });
 
+    $('#continue').click(function(){
+        $(this).hide();
+        $('#reset').hide();
+        $('#pause').show();
+        setTimerInterval();
+    });
 
+    $('#reset').click(function(){
+        $(this).hide();
+        $('#start').show();
+        $('#continue').hide();
+        $('#minutes').text('0');
+        $('#seconds').text('00');
+        seconds = 0;
+        minutes = 0;
+    })
 
+    function setTimerInterval(){
+        timer = setInterval(function(){
+            // console.log('this is happening every second');
+            seconds++;
+            if(seconds == 60){
+                minutes++;
+                seconds = 0;
+                $('#minutes').text(minutes);
+            }
+            if(seconds < 10){
+                $('#seconds').text('0'+seconds);
+            } else {
+                $('#seconds').text(seconds);
+            }
+
+        }, 1000);
+    }
 
 });
